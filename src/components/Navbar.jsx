b37e023dc58578553b4bdc3cac6b85cc814151a1
Abdulrahman Gaoba <88571592+AbdulrahmanGooba@users.noreply.github.com>
2023-03-15 21:36:44 +0500

    done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,10 @@
-import { Stack, Typography } from "@mui/material";
+import { Stack } from "@mui/material";
 import { Link } from "react-router-dom";
 
 import { logo } from "../utils/constants";
 import { SearchBar } from "./";
+// import logo  from '../imgs/logo.png';
+
 
 const Navbar = () => (
   <Stack direction="row" alignItems="center" p={2} sx={{ position:  "sticky", background: '#003049', top: 0, justifyContent: "space-between" }}>
